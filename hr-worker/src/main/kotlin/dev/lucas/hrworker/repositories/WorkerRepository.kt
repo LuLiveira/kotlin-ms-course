1b29678413577922863a07509a1fdb2a57e77376
Lucas Silva <lcsd.lucas@gmail.com>
2022-03-16 23:15:11 -0300

    add property to application.properties

--- hr-worker/src/main/kotlin/dev/lucas/hrworker/repositories/WorkerRepository.kt
+++ hr-worker/src/main/kotlin/dev/lucas/hrworker/repositories/WorkerRepository.kt
@@ -2,5 +2,7 @@ package dev.lucas.hrworker.repositories
 
 import dev.lucas.hrworker.entities.Worker
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface WorkerRepository : JpaRepository<Worker, Long>
