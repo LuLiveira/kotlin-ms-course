01d7bf389ff770cdc8ac8203701a9e034a503c46
Lucas Silva <lcsd.lucas@gmail.com>
2022-03-16 22:51:21 -0300

    Project hr-payroll created

---
+++ hr-payroll/src/main/kotlin/dev/lucas/hrpayroll/HrPayrollApplication.kt
@@ -0,0 +1,11 @@
+package dev.lucas.hrpayroll
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HrPayrollApplication
+
+fun main(args: Array<String>) {
+	runApplication<HrPayrollApplication>(*args)
+}
---
+++ hr-payroll/src/test/kotlin/dev/lucas/hrpayroll/HrPayrollApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.lucas.hrpayroll
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HrPayrollApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
